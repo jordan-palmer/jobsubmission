7ed8dedb75d3962766d20bdcfcdb424d99b384b2
Jordan Palmer <quarkyjordan@gmail.com>
2019-01-26 19:22:35 +0000

    added documentation

--- JobSub.py
+++ JobSub.py
@@ -4,12 +4,14 @@ import subprocess as sub
 class jobSubmitter(object):
 
     def __init__(self,sourceDir,workDir,cmd,jobName):
+        #Constructer - set member variables here
         self.sourceDir = sourceDir
         self.workDir = wowrkDir
         self.cmd = cmd
-        slef.jobName = jobName
+        self.jobName = jobName
 
-    def writeJobBash(self,inFile, ):
+    def writeJobBash(self,inFile):
+        #Function that writes a shell script to a file which will be qsubbed 
         f = open(self.jobName,"w+")
         line1 = "#!/bin/bash"
         line2 = "cd" + " " + self.workDir
@@ -17,12 +19,12 @@ class jobSubmitter(object):
         line4 = "echo sourced config"
         line5 = "cd" + " " + cmdDir
         line6 = "../" + self.cmd + " " + inFile
-    
-    
+        #This line writes the lines in a column 
         f.write('%s\n%s\n%s\n%s\n%s\n%s\n'%(line1,line2,line3,line4,line5,line6))
 
 
     def jobSubmit(err_file, log_file):
+        #Function to qsub jobs 
         os.system("chmod +x" + " " + self.jobName)
         sub.call(["qsub","-q","long","-l","pvmem=8gb","-o",log_file,"-e",err_file, self.jobName])
         #sub.call(["./"+jobName])
