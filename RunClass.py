a229fd43a2ae56d854aee50aa0362a9ec56f09a1
Jordan Palmer <quarkyjordan@gmail.com>
2019-02-24 22:22:01 +0000

    new Format

--- RunClass.py
+++ RunClass.py
@@ -1,19 +1,28 @@
+#=====================================================================
+#Example 1                  -  Updated : Jordan Palmer : 24-02-19
+#=====================================================================
+#Simple job submission script example. 
+# 1. Creates an object of the class (x)
+# 2. Writes a shell script
+# 3. Submits job on the farm
+# Edit the variables below in the same format
+#=====================================================================
+
 import jobsubmission.JobSub 
 from jobsubmission.JobSub import *
 
-inFile="text.txt"
-source = "/scratch3/jpalmer/PhD/JP-LEDs2/BACCARAT/setup.sh"
-#workdir = "/scratch3/jpalmer/PhD/JP-LEDs2/BACCARAT"
-workdir = "/hadoop/data/jpalmer/outputfiles/TPCtopsimdata/"
-command = "/scratch3/jpalmer/PhD/JP-LEDs2/BACCARAT/build.x86_64-slc6-gcc48-opt/bin/BaccMCTruth"
-myfile = "/hadoop/data/jpalmer/outputfiles/TPCtopsimdata/lz_mdc2_TPCtop_100_5000_JP_1004367627.root"
-#myfile = "lz_mdc3_Na22_JP_100_ALL_2.root"
-errorfile = "errfile.txt"
-logfile ="logfile.txt"
-shellname = "LEDTEST"
-jobdirname = "RootTOMCTruthNa22"
-#source,"shelltest",command,myfile,workdir
+source = "/scratch3/jpalmer/PhD/JP-LEDs2/BACCARAT/setup.sh" #Full path to environment you would like to source 
+workdir = "/scratch3/jpalmer/PhD/JP-LEDs2/BACCARAT" #Working directory, where job directory will be created
+command = "/scratch3/jpalmer/PhD/JP-LEDs2/BACCARAT/build.x86_64-slc6-gcc48-opt/bin/BaccRootConverter" #command of job, i.e "./executable" , "root -l" 
+myfile = "lz_mdc3_Na22_JP_100_1003951237.bin" #Target file you wish to act 'command' on
+errorfile = "errfile.txt" #Name of error file
+logfile = "logfile.txt" #name of log file
 
-x = jobsubmission.jobSubmitter(source,"TESTDIR", command, myfile, workdir)
+# 1
+x = jobsubmission.jobSubmitter(source,"EXAMPLE1",  
+                               command, myfile, workdir)
+# 2
 jobsubmission.jobSubmitter.writeJobBash(x)
-jobsubmission.jobSubmitter.jobSubmit(x,errorfile,logfile,"medium","4gb")
+# 3
+jobsubmission.jobSubmitter.jobSubmit(x, errorfile, 
+                                     logfile,"medium","4gb")
