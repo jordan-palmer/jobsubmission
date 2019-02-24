338022f253e50c813e9c436f32b0dd9e0b041806
Jordan Palmer <quarkyjordan@gmail.com>
2019-02-24 22:40:34 +0000

    new format

--- HelloFarm.py
+++ HelloFarm.py
@@ -1,3 +1,15 @@
+#=====================================================================#                                       
+#HelloFarm                  -  Updated : Jordan Palmer : 24-02-19     #          
+#=====================================================================#                                                                           
+#Simple HelloFarm job submission script example.                      #                                                                   
+# 1. Creates an object of the class (x)                               #                                                                                 
+# 2. Writes a shell script                                            #                                                                         
+# 3. Submits job on the farm                                          #                                                                                  
+# Edit the variables below in the same format                         #
+# This program writes 'Hello Farm' to a text file. This should be used#
+# to test that your code runs                                         #                                                              
+#=====================================================================#
+
 import jobsubmission.JobSub
 from jobsubmission.JobSub import *
 
@@ -7,7 +19,6 @@ command = "echo 'hello farm' >> "   #command you want to run, eg 'root -l'
 myfile = "HelloFarm.txt"   #input file, file you act command on
 errorfile = "errfile_hello.txt"   #error file
 logfile ="logfile_hello.txt"   #log file
-#jobdirname = "HelloFarmDir"  #directory in which you want your job to run in 
 jobname = "HelloFarm"   #jobname 
 joblength = "short" #Sets the allocated time: long=500hours, medium=20hours, short=20mins
 jobmemory = "500mb" #sets allocated memory. Make sure you choose the lowest memory possible!
@@ -15,9 +26,3 @@ jobmemory = "500mb" #sets allocated memory. Make sure you choose the lowest memo
 x = jobsubmission.jobSubmitter(source,"HELLOWORLD",command, myfile,workdir) #object of the class
 jobsubmission.jobSubmitter.writeJobBash(x) #creates .sh script to be run on farm
 jobsubmission.jobSubmitter.jobSubmit(x,errorfile,logfile,joblength,jobmemory) #qsubs on farm
-
-
-
-#x = jobsubmission.jobSubmitter(source,"TESTDIR", command, myfile, workdir)
-#jobsubmission.jobSubmitter.writeJobBash(x)
-#jobsubmission.jobSubmitter.jobSubmit(x,errorfile,logfile,"medium","4gb") 
